f6b1ef07bc89a277b64912529d2b7e185af55648
mohammad.hamidimoghadam <mohammad.hamidimoghadam@partsoftware.com>
2024-08-01 12:01:56 +0330

    fix: displaying the user tel in header (caused by js malfunctioning)

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -3,7 +3,7 @@ const userTel = document.querySelector(".user-info__tel");
 const user = JSON.parse(User.getUserFromLocalStorage());
 
 if (user) {
-    userTel.textContent = parseInt(user.tel).toLocaleString("fa-IR").replace(/٬/g, "");
+    userTel.textContent = "۰" + parseInt(user.tel).toLocaleString("fa-IR").replace(/٬/g, "");
 } else {
     userTel.textContent = "- - - - - -";
 }
