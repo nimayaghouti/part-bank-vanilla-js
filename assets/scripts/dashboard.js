fd459b94a61979ac99b7bea5cfbf63e248cc30b2
mohammad.hamidimoghadam <mohammad.hamidimoghadam@partsoftware.com>
2024-08-06 11:49:12 +0330

    fix: maxLength specified for phone input

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,5 +1,4 @@
 const phoneDisplayElement = document.querySelector(".user-info__phone");
 const storedUser = Storage.getItem('user');
 
-const formattedPhone = storedUser?.phone;
-phoneDisplayElement.textContent = formattedPhone;
+phoneDisplayElement.textContent = storedUser?.phone;
