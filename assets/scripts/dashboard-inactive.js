2c497330f7840044f414049f5d98aea841be22c8
mohammad.hamidimoghadam <mohammad.hamidimoghadam@partsoftware.com>
2024-08-07 12:43:53 +0330

    feat: confirm info page desined + page flow logic implemented

---
+++ assets/scripts/dashboard-inactive.js
@@ -0,0 +1,10 @@
+const phoneDisplayElement = document.querySelector('.user-info__phone');
+const createAccountButton = document.querySelector('.dialog__button');
+
+const storedUser = Storage.getItem('user');
+
+phoneDisplayElement.textContent = storedUser?.phone;
+
+createAccountButton.addEventListener('click', () => {
+  window.location = 'pages/create-account/personal-info.html';
+});
