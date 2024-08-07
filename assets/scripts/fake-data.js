9e6efc6b9efbe75ae188c79581cd12beab92b541
mohammad.hamidimoghadam <mohammad.hamidimoghadam@partsoftware.com>
2024-08-07 13:46:53 +0330

    feat: active state for dashboard designed + fake data

---
+++ assets/scripts/fake-data.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    id: 1,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 2,
+    type: "deposit",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 3,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 4,
+    type: "deposit",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 5,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 6,
+    type: "deposit",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 7,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 8,
+    type: "deposit",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 9,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 10,
+    type: "deposit",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+  {
+    id: 11,
+    type: "withdraw",
+    dateTime: "Sat Jul 27 2024 16:19:02 GMT+0330 (Iran Standard Time)",
+    Amount: "21,200,000",
+  },
+];
