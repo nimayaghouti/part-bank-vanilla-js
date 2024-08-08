c3562e3c98c083791ca775b2e86956da157cc030
nima.yaghouti <nima.yaghouti@partsoftware.com>
2024-08-08 10:20:29 +0330

    feat: mock api raplaced with real api and save to local storage functionlity moved to it's specefic function

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,52 +1,53 @@
-import instance from "../services/apiService.js";
+import instance from '../services/apiService.js';
 
-const phoneDisplayElement = document.querySelector(".user-info__phone");
+const phoneDisplayElement = document.querySelector('.user-info__phone');
 
-const dialog = document.querySelector(".info__dialog");
+const dialog = document.querySelector('.info__dialog');
 
-// const 
+// const
 
-const table = document.querySelector(".transactions__table");
-const tableBody = document.querySelector(".table__body");
-const paginationPrevious = document.querySelector(".pagination__previous");
-const paginationNext = document.querySelector(".pagination__next");
-const pageButtonsWrapper = document.querySelector(".pagination__dynamic-buttons-wrapper");
-
+const table = document.querySelector('.transactions__table');
+const tableBody = document.querySelector('.table__body');
+const paginationPrevious = document.querySelector('.pagination__previous');
+const paginationNext = document.querySelector('.pagination__next');
+const pageButtonsWrapper = document.querySelector(
+  '.pagination__dynamic-buttons-wrapper'
+);
 
 // const storedUser = Storage.getItem('user');
 // phoneDisplayElement.textContent = storedUser?.phone;
 
-
 const setDashboardStatus = () => {
-    const hashtag = window.location.hash;
-    if (hashtag === "#active") {
-        dialog.classList.add('dialog_active-dashboard');
-        table.classList.add('table_active-dashboard')
-    }
-}
+  const hashtag = window.location.hash;
+  if (hashtag === '#active') {
+    dialog.classList.add('dialog_active-dashboard');
+    table.classList.add('table_active-dashboard');
+  }
+};
 setDashboardStatus();
 
 const TEMP_USER_ID = '1';
 const getTransaction = async () => {
-    const { data } = await instance.get(`transactions/${TEMP_USER_ID}`);
-    const status = data.response.data.status;
+  const { data } = await instance.get(`transactions/${TEMP_USER_ID}`);
+  const status = data.response.data.status;
 
-    if (status === "success") {
-        return data.response.data.results;
-    } else {
-        return [];
-    }
+  if (status === 'success') {
+    return data.response.data.results;
+  } else {
+    return [];
+  }
 };
 const tableData = await getTransaction();
 
-
 // creating tr tags based on data //////////////////////////////
-const tableRowsArray = tableData.map(rowData => {
-    const type = rowData.type === "deposit" ? "واریز" : "برداشت";
-    const date = new Date(rowData.date).toLocaleString("fa-IR", "numeric");
-    const amount = parseInt(rowData.amount.split(',').join('')).toLocaleString("fa-IR");
+const tableRowsArray = tableData.map((rowData) => {
+  const type = rowData.type === 'deposit' ? 'واریز' : 'برداشت';
+  const date = new Date(rowData.date).toLocaleString('fa-IR', 'numeric');
+  const amount = parseInt(rowData.amount.split(',').join('')).toLocaleString(
+    'fa-IR'
+  );
 
-    return (`
+  return `
         <tr>
             <td>
                 <img src="./assets/svg/icons/dashboard/money-${rowData.type}.svg" />
@@ -55,9 +56,8 @@ const tableRowsArray = tableData.map(rowData => {
             <td>${date}</td>
             <td>${amount}</td>
         </tr>
-    `);
+    `;
 });
-
 
 // implementing the pagination functionality //////////////////////////////
 let currentPage = 1;
@@ -66,58 +66,76 @@ const lastPage = Math.ceil(tableRowsArray.length / maxItems); // totalPages
 
 // refresh table rows of data
 function refreshTbody() {
-    const currentTbodyContent = tableRowsArray.slice(currentPage * maxItems - maxItems, currentPage * maxItems);
-    tableBody.innerHTML = "";
-    currentTbodyContent.map(trTag => tableBody.insertAdjacentHTML('beforeend', trTag));
+  const currentTbodyContent = tableRowsArray.slice(
+    currentPage * maxItems - maxItems,
+    currentPage * maxItems
+  );
+  tableBody.innerHTML = '';
+  currentTbodyContent.map((trTag) =>
+    tableBody.insertAdjacentHTML('beforeend', trTag)
+  );
 }
 
 // refresh styles of number buttons
 function refreshButtons(remove = false) {
-    const selectedPageButton = document.querySelector(`#pagination__dynamic-button-${currentPage}`);
-    if (!selectedPageButton) return;
-    if (remove) {
-        selectedPageButton.classList.remove('pagination__dynamic-button_selected');
-    } else {
-        selectedPageButton.classList.add('pagination__dynamic-button_selected');
-    }
+  const selectedPageButton = document.querySelector(
+    `#pagination__dynamic-button-${currentPage}`
+  );
+  if (!selectedPageButton) return;
+  if (remove) {
+    selectedPageButton.classList.remove('pagination__dynamic-button_selected');
+  } else {
+    selectedPageButton.classList.add('pagination__dynamic-button_selected');
+  }
 }
 
 // refreshing
 function refresh(cp) {
-    refreshButtons(true); // cp has not changed yet
-    currentPage = cp; // cp changes
-    refreshTbody();
-    refreshButtons();
+  refreshButtons(true); // cp has not changed yet
+  currentPage = cp; // cp changes
+  refreshTbody();
+  refreshButtons();
 }
 
 // creating and adding number buttons
 for (let i = 1; i <= lastPage; i++) {
-    const pageButton = document.createElement('button');
+  const pageButton = document.createElement('button');
 
-    pageButton.setAttribute("id", `pagination__dynamic-button-${i}`);
-    pageButton.setAttribute("class", `pagination__dynamic-button`);
-    pageButton.innerHTML = i.toLocaleString("fa-IR");
-    pageButton.addEventListener('click', () => {
-        refresh(i);
-    })
+  pageButton.setAttribute('id', `pagination__dynamic-button-${i}`);
+  pageButton.setAttribute('class', `pagination__dynamic-button`);
+  pageButton.innerHTML = i.toLocaleString('fa-IR');
+  pageButton.addEventListener('click', () => {
+    refresh(i);
+  });
 
-    pageButtonsWrapper.append(pageButton);
+  pageButtonsWrapper.append(pageButton);
 }
 
 // arrow buttons
 paginationPrevious.addEventListener('click', () => {
-    if (currentPage === 1) return;
-    refresh(currentPage - 1)
+  if (currentPage === 1) return;
+  refresh(currentPage - 1);
 });
 paginationNext.addEventListener('click', () => {
-    if (currentPage === lastPage) return;
-    refresh(currentPage + 1)
+  if (currentPage === lastPage) return;
+  refresh(currentPage + 1);
 });
 
 // initial refresh to put every thing together
 if (!tableData.length) {
-    tableBody.insertAdjacentHTML('beforeend', "<p style='padding: 5rem 0;'>هیچ داده ای وجود ندارد!</p>");
-    document.querySelector('.button-container').style.display = 'none';
+  tableBody.insertAdjacentHTML(
+    'beforeend',
+    "<p style='padding: 5rem 0;'>هیچ داده ای وجود ندارد!</p>"
+  );
+  document.querySelector('.button-container').style.display = 'none';
 } else {
-    refresh(currentPage);
+  refresh(currentPage);
 }
+
+// const navigateToDashboard = async (id) => {
+//     const { data } = await instance.get(`deposit-account/${id}`);
+//     const status = data.response.data.status;
+//     const dashboardState = status === 'success' ? 'active' : 'inactive';
+
+//     window.location = `../../dashboard.html#${dashboardState}`;
+//   };
